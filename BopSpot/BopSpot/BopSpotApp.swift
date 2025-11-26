4c5644105ec7778e356578d0db7acdff822de9a7
Anil Aygun <anlaygun1@gmail.com>
2025-11-26 12:16:56 +0300

    first commit

---
+++ BopSpot/BopSpot/BopSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BopSpotApp.swift
+//  BopSpot
+//
+//  Created by Anıl Aygün on 26.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BopSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
